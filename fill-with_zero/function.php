dba61391f4605a55dbd2b049a06e9b604bfe6675
jkail <boutaultbenoit@yahoo.fr>
2015-09-23 23:45:11 +0200

    Ecriture de la réflexion

---
+++ fill-with_zero/function.php
@@ -0,0 +1,25 @@
+<?php
+/*
+1- On va creer un fonction qui va ajouter des zeros devant une chaine
+soit $lenght la taille de la chaine et $chaine la chaine à compléter.
+Ainsi pour $lenght=4 et $chaine=25 on la fonction devra renvoyer 0025
+
+2- si la chaine est plus longue que lenght ou n'est pas une serie de chiffre -> ca renvoie false
+3- systeme :
+on calcule la tailel de la chaine.
+On soustrait cette valeur à la taille lenght
+On ajoute autant de zero que le resultat de la soustraction le demande devant la chaine (s'il faut, on l'inverse et on rajoute le tout apres avant de réinverser
+
+Il sera probableme necessaire de mettre la chaine dans un array (explode/implode)
+
+
+Version 2 : on rajoute des zero devant $chaine et on recupere le nombre de caractere de taille $lenght
+
+Alright guys let's do this !
+-Leroy Jenkins
+
+*/
+
+
+
+?>
